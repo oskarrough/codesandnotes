8b0f13beabc1d1ad485499e8ab15e12deeef185d
Oskar Rough <oskarrough@gmail.com>
2014-01-11 22:00:48 +0100

    Add simple markdown and pdf build processes

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // load all grunt tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      distribution: ['dist']
+    },
+
+    // Markdown to HTML
+    markdown: {
+      all: {
+        options: {
+          gfm: true,
+          highlight: 'auto'
+        },
+        files: [
+          {
+            expand: true,
+            src: 'Chapters/**/*.md',
+            //src: 'chapters/*.md',
+            dest: 'dist/html',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+
+    // Markdown to PDF
+    markdownpdf: {
+      options: {
+        concatFiles: true,
+        paperFormat: 'A4',
+        renderDelay: 2000,
+        paperBorder: '1cm'
+      },
+      files: {
+        src: 'Chapters/**/*.md',
+        dest: 'dist/pdf'
+      }
+    }
+  })
+
+  grunt.registerTask('default', [
+    'clean',
+    'markdown',
+    'markdownpdf'
+  ])
+}
