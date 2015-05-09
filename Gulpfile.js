5b3e97b0ab6291b556311931a91386241ba33c4e
Oskar Rough <oskarrough@gmail.com>
2015-05-09 23:42:20 +0200

    Improve build system

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,17 +5,32 @@ var autoprefixer = require('autoprefixer-core');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var compression = require('compression');
-var matter = require('gray-matter');
-var insert = require('gulp-insert');
-var remarkable = require('gulp-remarkable');
+var remarkable = require('gulp-remarkable'); // this doesn't seem to work with gulp-load-plugins
+// var header = require('gulp-header');
+var toc = require('gulp-toc');
 
 gulp.task('markdown', function() {
-  return gulp.src('content/**/*.md')
-    .pipe(remarkable({
-    	preset: 'commonmark',
-    	typographer: true
-    }))
-    .pipe(gulp.dest('dist'));
+	return gulp.src('content/**/*.md')
+
+		// convert to html
+		.pipe(remarkable({
+			preset: 'commonmark',
+			typographer: true
+		}))
+
+		// insert toc
+		.pipe($.insert.prepend('<!-- toc -->\n'))
+		.pipe(toc())
+
+		// wrap layout
+		.pipe($.wrap({ src: 'app/templates/layout.html' }))
+		.pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('app/*.html')
+		.pipe($.wrap({ src: 'app/templates/layout.html' }))
+		.pipe(gulp.dest('.tmp'));
 });
 
 // http://emberjs.jsbin.com/nakukoweme/1/edit?html,output
@@ -42,41 +57,16 @@ gulp.task('styles', function () {
 		.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('.tmp/styles'))
 		.pipe($.filter('**/*.css')) // Filtering stream to only css files. Needed for browser-sync css injection
-		.pipe(reload({stream: true}));
+		.pipe(reload({ stream: true }));
 });
 
-// Lint all scripts except those inside scripts/vendor
-gulp.task('jshint', function () {
-	return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
-		.pipe(reload({stream: true, once: true}))
-		.pipe($.jshint())
-		.pipe($.jshint.reporter('jshint-stylish'))
-		.pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
-});
-
-// compiles jade templates into html
-gulp.task('templates', function() {
-	return gulp.src([
-		'app/templates/**/*.jade',
-		'!app/templates/partials/**/*.jade'
-	])
-		/* the jade file can define title or order; put those at file.data */
-		// .pipe($.data(function(file) { return matter(String(file.contents)); }))
-		// .pipe($.nav())
-		.pipe($.jade({ pretty: true }))
-		.pipe(gulp.dest('.tmp'));
-});
-
-// compiles templates and styles, then minifies and concatenate all assets
-gulp.task('html', ['templates', 'styles'], function () {
-	var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
-	return gulp.src(['.tmp/*.html'])
-		.pipe(assets)
-		.pipe($.if('*.js', $.uglify()))
-		.pipe($.if('*.css', $.csso()))
-		.pipe(assets.restore())
-		.pipe($.useref())
-		.pipe(gulp.dest('dist'));
+gulp.task('scripts', function () {
+	return gulp.src('app/scripts/**/*.js')
+		.pipe($.sourcemaps.init())
+		.pipe($.babel())
+		// .pipe($.concat('main.js'))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('.tmp/scripts/'));
 });
 
 // moves images to dist (no optimization because I do it manually)
@@ -86,20 +76,10 @@ gulp.task('images', function () {
 		.pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('extras', function () {
-	return gulp.src([
-		'app/*.*',
-		'!app/*.html',
-		'!app/*.jade'
-	], {
-		dot: true
-	}).pipe(gulp.dest('dist'));
-});
-
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 // starts a server for development
-gulp.task('serve', ['templates', 'styles'], function () {
+gulp.task('serve', ['html', 'markdown', 'styles', 'scripts'], function () {
 	browserSync({
 		notify: false,
 		port: 9000,
@@ -112,23 +92,20 @@ gulp.task('serve', ['templates', 'styles'], function () {
 		}
 	});
 
-	// watch for changes
-	gulp.watch([
-		// 'app/*.html',
-		'app/scripts/**/*.js',
-		'app/images/**/*'
-	]).on('change', reload);
-
-	gulp.watch('app/templates/**/*.jade', ['templates', reload]);
-	gulp.watch('app/styles/**/*.scss', ['styles']);
+	// watch for changes and run tasks
+	gulp.watch(['app/*.html', 'app/templates/*.html'], ['html', reload]);
+	gulp.watch('content/**/*.md', ['markdown', reload]);
+	gulp.watch('app/scripts/**/*.js', ['scripts', reload]);
+	gulp.watch('app/styles/**/*.scss', ['styles']); // 'styles' does reload
 });
 
 // shortcut for serve
 gulp.task('s', ['serve']);
 
-gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
-	return gulp.src('dist/**/*')
-		.pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['markdown', 'styles', 'scripts'], function() {
+	return gulp.src('.tmp/**/*')
+		.pipe(gulp.dest('dist'))
+		.pipe($.size({ title: 'build', gzip: true }));
 });
 
 gulp.task('default', ['clean'], function () {
