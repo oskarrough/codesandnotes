1345166467eb1aca3d9db2c02d2c1d1e70da113f
Oskar Rough <oskarrough@gmail.com>
2015-05-09 16:01:51 +0200

    Remove old templating attempts and add proper markdown compiling

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,23 +5,37 @@ var autoprefixer = require('autoprefixer-core');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var compression = require('compression');
-var matter = require('jade-var-matter');
+var matter = require('gray-matter');
+var insert = require('gulp-insert');
+var remarkable = require('gulp-remarkable');
 
+gulp.task('markdown', function() {
+  return gulp.src('content/**/*.md')
+    .pipe(remarkable({
+    	preset: 'commonmark',
+    	typographer: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
 
-// Styles with Libsass
+// http://emberjs.jsbin.com/nakukoweme/1/edit?html,output
+gulp.task('tree', function () {
+	return gulp.src('content/**/*.md')
+		.pipe($.fileTree())
+		// .pipe($.rename({
+		// 	extname: '.jade'
+		// }))
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('styles', function () {
 	return gulp.src('app/styles/*.scss')
 		.pipe($.sourcemaps.init())
 		.pipe($.sass({
-			outputStyle: 'nested',
+			outputStyle: 'expanded',
 			precision: 10,
 			includePaths: ['.'],
-			require: 'susy',
-			onError: function(err) {
-				$.notify().write(err);
-				// console.error.bind(console, 'Sass error:')
-			}
-		}))
+		}).on('error', $.sass.logError))
 		.pipe($.postcss([
 			autoprefixer({ browsers: ['last 2 version', 'android 4', 'ios 7', 'ie 10']})
 		]))
@@ -40,29 +54,21 @@ gulp.task('jshint', function () {
 		.pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
-// Compile .jade into .html in the .tmp dir
-// gulp.task('views', function () {
-// 	return gulp.src('app/*.jade')
-// 		.pipe($.jade({pretty: true}))
-// 		.on('error', $.notify.onError(function (error) {
-// 			return 'An error occurred while compiling jade.\nLook in the console for details.\n' + error;
-// 		}))
-// 		.pipe(gulp.dest('.tmp'));
-// });
-
-gulp.task('views', function() {
+// compiles jade templates into html
+gulp.task('templates', function() {
 	return gulp.src([
 		'app/templates/**/*.jade',
 		'!app/templates/partials/**/*.jade'
 	])
 		/* the jade file can define title or order; put those at file.data */
-		.pipe($.data(function(file) { return matter(String(file.contents)); }))
-		.pipe($.nav())
+		// .pipe($.data(function(file) { return matter(String(file.contents)); }))
+		// .pipe($.nav())
 		.pipe($.jade({ pretty: true }))
 		.pipe(gulp.dest('.tmp'));
 });
 
-gulp.task('html', ['views', 'styles'], function () {
+// compiles templates and styles, then minifies and concatenate all assets
+gulp.task('html', ['templates', 'styles'], function () {
 	var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
 	return gulp.src(['.tmp/*.html'])
 		.pipe(assets)
@@ -73,6 +79,7 @@ gulp.task('html', ['views', 'styles'], function () {
 		.pipe(gulp.dest('dist'));
 });
 
+// moves images to dist (no optimization because I do it manually)
 gulp.task('images', function () {
 	return gulp.src('app/images/**/*')
 		.pipe($.cache())
@@ -91,8 +98,8 @@ gulp.task('extras', function () {
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
-gulp.task('s', ['serve']);
-gulp.task('serve', ['views', 'styles'], function () {
+// starts a server for development
+gulp.task('serve', ['templates', 'styles'], function () {
 	browserSync({
 		notify: false,
 		port: 9000,
@@ -112,9 +119,12 @@ gulp.task('serve', ['views', 'styles'], function () {
 		'app/images/**/*'
 	]).on('change', reload);
 
-	gulp.watch('app/templates/**/*.jade', ['views', reload]);
+	gulp.watch('app/templates/**/*.jade', ['templates', reload]);
 	gulp.watch('app/styles/**/*.scss', ['styles']);
 });
+
+// shortcut for serve
+gulp.task('s', ['serve']);
 
 gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
 	return gulp.src('dist/**/*')
