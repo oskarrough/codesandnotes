f35bbd619ac9a84cefbac7fe939db32ba36043ba
Oskar Rough <oskarrough@gmail.com>
2015-05-13 23:13:02 +0200

    Update build setup to better integrate gulp + hugo

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,29 +5,33 @@ var autoprefixer = require('autoprefixer-core');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var compression = require('compression');
-var remarkable = require('gulp-remarkable'); // this doesn't seem to work with gulp-load-plugins
+// var remarkable = require('gulp-remarkable'); // this doesn't seem to work with gulp-load-plugins
 // var header = require('gulp-header');
 
-gulp.task('markdown', function() {
-	return gulp.src('content/**/*.md')
+var base = 'app/';
 
-		// convert to html
-		.pipe(remarkable({
-			preset: 'commonmark',
-			typographer: true
-		}))
-
-		// insert toc
-		.pipe($.insert.prepend('<!-- toc -->\n'))
-		.pipe(toc())
-
-		// wrap layout
-		.pipe($.wrap({ src: 'app/templates/layout.html' }))
-		.pipe(gulp.dest('.tmp'));
+// Builds all content
+gulp.task('hugo', function (cb) {
+	return exec('hugo --verbose=true', function (err, stdout, stderr) {
+		console.log(stdout); // Hugo output
+		console.log(stderr); // Debugging feedback
+		cb(err);
+	});
 });
 
+// Builds and deploys to dev domain
+// (as specified in app/static/CNAME)
+gulp.task('deploy', ['default'], function (cb) {
+	exec('surge dist', function (err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
+
+// Sass, sourcemaps and autoprefixer
 gulp.task('styles', function () {
-	return gulp.src('app/styles/*.scss')
+	return gulp.src(base + 'styles/*.scss')
 		.pipe($.sourcemaps.init())
 		.pipe($.sass({
 			outputStyle: 'expanded',
@@ -44,7 +48,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('scripts', function () {
-	return gulp.src('app/scripts/**/*.js')
+	return gulp.src(base + 'scripts/**/*.js')
 		.pipe($.sourcemaps.init())
 		.pipe($.babel())
 		// .pipe($.concat('main.js'))
@@ -55,33 +59,73 @@ gulp.task('scripts', function () {
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 // starts a server for development
-gulp.task('serve', ['markdown', 'styles', 'scripts'], function () {
+gulp.task('serve', ['hugo', 'styles', 'scripts'], function () {
 	browserSync({
 		server: {
-			baseDir: ['.tmp', 'app'],
-			routes: {
-				'/bower_components': 'bower_components'
-			},
+			baseDir: ['.tmp', 'app', 'dist'],
 			middleware: compression()
 		}
 	});
 
 	// watch for changes and run tasks
 	// gulp.watch(['app/*.html', 'app/templates/*.html'], ['html', reload]);
-	// gulp.watch('content/**/*.md', ['markdown', reload]);
-	gulp.watch('app/scripts/**/*.js', ['scripts', reload]);
-	gulp.watch('app/styles/**/*.scss', ['styles']); // 'styles' does reload
+	gulp.watch('content/**/*.md', ['hugo', reload]);
+	gulp.watch(base + 'layouts/**/*.html', ['hugo', reload]);
+	gulp.watch(base + 'scripts/**/*.js', ['scripts', reload]);
+	gulp.watch(base + 'styles/**/*.scss', ['styles']); // 'styles' does reload
 });
 
 // shortcut for serve
 gulp.task('s', ['serve']);
 
-gulp.task('build', ['markdown', 'styles', 'scripts'], function() {
+// this builds content, styles and scripts
+gulp.task('build', ['hugo', 'styles', 'scripts'], function() {
 	return gulp.src('.tmp/**/*')
 		.pipe(gulp.dest('dist'))
 		.pipe($.size({ title: 'build', gzip: true }));
 });
 
+// cleans, then builds - use this
 gulp.task('default', ['clean'], function () {
 	gulp.start('build');
 });
+
+// gulp.task('markdown', function() {
+// 	return gulp.src('content/**/*.md')
+
+// 		// convert to html
+// 		.pipe(remarkable({
+// 			preset: 'commonmark',
+// 			typographer: true
+// 		}))
+
+// 		// insert toc
+// 		.pipe($.insert.prepend('<!-- toc -->\n'))
+// 		.pipe(toc())
+
+// 		// wrap layout
+// 		.pipe($.wrap({ src: 'app/templates/layout.html' }))
+// 		.pipe(gulp.dest('.tmp'));
+// });
+//
+
+// gulp.task('minify-css', ['styles'], function() {
+//   return gulp.src(dist.assets)
+//     .pipe(plugins.minifyCss())
+//     .pipe(gulp.dest(dist.assets))
+// })
+
+// gulp.task('minify-js', ['js'], function() {
+//   return gulp.src(dist.assets)
+//     .pipe(plugins.uglify())
+//     .pipe(gulp.dest(dist.assets))
+// })
+
+// Continously builds all content and starts a server
+// gulp.task('hugo:server', function (cb) {
+// 	exec('hugo server --watch --config=./app/config.yaml --source=./app', function (err, stdout, stderr) {
+// 		console.log(stdout);
+// 		console.log(stderr);
+// 		cb(err);
+// 	});
+// });
---
+++ content/Content-management/statamic.md
@@ -0,0 +1,13 @@
+---
+title: Statamic
+---
+
+[Statamic](http://statamic.com/) is a pretty useful filed-based CMS system with good ideas for the future. The two major draws for me being total version control and close to no start-up time.
+
+You can store and manage all your content with files (could be text, markdown, html) and Statamic adds variables and templating with PHP and Yaml. I wonder when (if) you hit the wall and wish you went with a more powerful system.
+
+### References
+
+- [statamic.com/blog](http://statamic.com/blog)
+- [tips.statamic.com](http://tips.statamic.com/)
+- [Statamicist.com](http://statamicist.com/)
